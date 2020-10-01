5866193d8704f717a8f2b94c78515a3dd505f072
khaledoon <45737002+khaledoon@users.noreply.github.com>
2020-10-01 23:38:59 +0200

    finished assignment page

---
+++ js/assingment.js
@@ -0,0 +1,26 @@
+
+$("#hints-table tr").hover(function(){
+    
+    let val=$('#hints-table tr').index(this);
+    let tabletd='#hints-table tr:nth-child('+(val+1)+') td';
+
+    $(tabletd).removeClass('none').removeClass('fadeout');
+    
+    $(tabletd).addClass('block').outerWidth();
+
+    $(tabletd).addClass('fadein');
+    
+})
+
+
+$("#hints-table tr").on("mouseleave",function(){
+    
+    let val=$('#hints-table tr').index(this);
+    let tabletd='#hints-table tr:nth-child('+(val+1)+') td';
+
+    $(tabletd).removeClass('block').removeClass('fadein');
+
+    $(tabletd).addClass('none').outerWidth();
+
+    $(tabletd).addClass('fadeout');
+})
