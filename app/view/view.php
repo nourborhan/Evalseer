f705936c06128314dde4f1cb609c1fed7492bb96
Ramez007 <45740184+Ramez007@users.noreply.github.com>
2020-10-21 20:32:36 +0200

    Setup For MVC Enviroment

---
+++ app/view/view.php
@@ -0,0 +1,13 @@
+<?php
+abstract class  View{
+    protected $model;
+    protected $controller;
+
+    public function __construct($controller, $model) {
+        $this->controller = $controller;
+        $this->model = $model;
+    }
+	
+    public abstract function output();
+}
+?>
