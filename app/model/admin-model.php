0b9dfe10d9de9bb35965af2a913c15392a0bb8b4
Ramez007 <45740184+Ramez007@users.noreply.github.com>
2020-10-24 22:25:00 +0200

    Fixed Courses DropDown

--- app/model/admin-model.php
+++ app/model/admin-model.php
@@ -23,53 +23,51 @@ class Admin extends model {
     function readCourseDetalis(){
         $sql="SELECT * FROM course;";
         $Result = mysqli_query($this->db->getConn(),$sql);
-
-        $instructoslist = '';
-        $sql2="SELECT user.Name,user.Title FROM user INNER JOIN courseedducator ON courseedducator.UserID=user.UserID and courseedducator.Primaryeducatorflag=1 ";
-        $Result2 = mysqli_query($this->db->getConn(),$sql2);
-        while($row2=$Result2->fetch_assoc())
-        {
-            $instructoslist .= "<li>".$row2["Name"]."</li>";
-        }
-
-        $TAlist = '';
-        $sql3="SELECT user.Name,user.Title FROM user INNER JOIN courseedducator ON courseedducator.UserID=user.UserID and courseedducator.Assistantflag =1 ";
-        $Result3 = mysqli_query($this->db->getConn(),$sql3);
-        while($row3=$Result3->fetch_assoc())
-        {
-            $TAlist .= "<li>".$row3["Name"]."</li>";
-        }
-
-
-
-
+        
         while($row=$Result->fetch_assoc())
         {
 
-            echo '<div id="'.$row["Coursecode"].'" style="display: none;">
-            <h5><strong>Course Title</strong></h5><span>'.$row["Name"].'</span>
-            <h5><strong>Course Code</strong></h5><span>'.$row["Coursecode"].'</span>
-            <h5><strong>Total Grade</strong></h5><span>'.$row["Grade"].'</span>
-            <h5><strong>Grade To Pass</strong></h5><span>'.$row["Gradetopass"].'</span>
-            <h5><strong>Start Date</strong></h5><span>'.$row["Startdate"].'</span>
-            <h5><strong>End Date</strong></h5><span>'.$row["Enddate"].'</span>
-            <h5><strong>Course Description</strong></h5>
-            <p>'.$row["Description"].'</p>
-            <div class="col-md-6">
-                <h5><strong>Assigned Proffessors</strong></h5>
-                <ul>
-                    '.$instructoslist.'
-                </ul>
-            </div>
-            <div class="col-md-6">
-                <h5><strong>Assigned TAs</strong></h5>
-                <ul>
-                '.$TAlist.'
-                </ul>
-            </div>
-            <button class="btn btn-primary" data-toggle="modal" data-target="#myModal'.$row["Coursecode"].'">Edit Course</button>
+            $instructoslist = '';
+            $sql2="SELECT user.Name,user.Title FROM user INNER JOIN courseedducator ON courseedducator.UserID=user.UserID and courseedducator.Primaryeducatorflag=1 and CourseID=".$row["CourseID"]."";
+            $Result2 = mysqli_query($this->db->getConn(),$sql2);
+            while($row2=$Result2->fetch_assoc())
+            {
+                $instructoslist .= "<li>".$row2["Name"]."</li>";
+            }
 
-        </div>
+            $TAlist = '';
+            $sql3="SELECT user.Name,user.Title FROM user INNER JOIN courseedducator ON courseedducator.UserID=user.UserID and courseedducator.Assistantflag =1 and CourseID=".$row["CourseID"]."";
+            $Result3 = mysqli_query($this->db->getConn(),$sql3);
+            while($row3=$Result3->fetch_assoc())
+            {
+                $TAlist .= "<li>".$row3["Name"]."</li>";
+            }
+
+            echo '
+            <div id="'.$row["Coursecode"].'" style="display: none;">
+                        <h5><strong>Course Title</strong></h5><span>'.$row["Name"].'</span>
+                        <h5><strong>Course Code</strong></h5><span>'.$row["Coursecode"].'</span>
+                        <h5><strong>Total Grade</strong></h5><span>'.$row["Grade"].'</span>
+                        <h5><strong>Grade To Pass</strong></h5><span>'.$row["Gradetopass"].'</span>
+                        <h5><strong>Start Date</strong></h5><span>'.$row["Startdate"].'</span>
+                        <h5><strong>End Date</strong></h5><span>'.$row["Enddate"].'</span>
+                        <h5><strong>Course Description</strong></h5>
+                        <p>'.$row["Description"].'</p>
+                        <div class="col-md-6">
+                            <h5><strong>Assigned Proffessors</strong></h5>
+                            <ul>
+                                '.$instructoslist.'
+                            </ul>
+                        </div>
+                        <div class="col-md-6">
+                            <h5><strong>Assigned TAs</strong></h5>
+                            <ul>
+                            '.$TAlist.'
+                            </ul>
+                        </div>
+                        <br><br>
+                        <button class="btn btn-primary" data-toggle="modal" data-target="#myModal'.$row["Coursecode"].'">Edit Course</button>
+
         
         
                                 <div class="modal fade" id="myModal'.$row["Coursecode"].'" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
@@ -138,7 +136,7 @@ class Admin extends model {
                                 </div>
         
         
-        ';
+         ';
         }
     }
 
